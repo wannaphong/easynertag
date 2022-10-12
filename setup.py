d39fa84c35c90589466f172e7c342f4041207f27
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-10-13 00:45:39 +0700

    EasyNERTag v0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt","r",encoding="utf-8-sig") as f:
 
 setup(
     name="EasyNERTag",
-    version="0.1",
+    version="0.2",
     description="Easy tagging for annotate NER corpus",
     long_description=readme,
     long_description_content_type="text/markdown",
